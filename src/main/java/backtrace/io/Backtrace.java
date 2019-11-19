1737b7ea160114a5defd2b84d3a8b68035d3e68b
Bartosz Litwiniuk <bartosz.litwiniuk@poczta.onet.pl>
2019-11-19 19:04:40 +0100

    Refactor BacktraceThread

---
+++ src/main/java/backtrace/io/Backtrace.java
@@ -0,0 +1,64 @@
+package backtrace.io;
+
+import backtrace.io.events.OnServerResponseEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class Backtrace {
+    private static final transient Logger LOGGER = LoggerFactory.getLogger(Backtrace.class);
+    private ConcurrentLinkedQueue<BacktraceMessage> queue;
+    private BacktraceDatabase database;
+    private final BacktraceConfig config;
+
+
+    public Backtrace(BacktraceConfig config, ConcurrentLinkedQueue<BacktraceMessage> queue)
+    {
+        this.database = BacktraceDatabase.init(config, queue);
+        this.config = config;
+        this.queue = queue;
+    }
+
+    void processBacktraceMessages(){
+        while(true){
+            try {
+                BacktraceMessage message = queue.poll();
+
+                if (message == null) {
+                    continue;
+                }
+
+                processSingleBacktraceMessage(message);
+            }
+            catch (Exception e){
+                LOGGER.error("Exception during pipeline for message from queue..", e);
+            }
+        }
+    }
+
+    private void processSingleBacktraceMessage(BacktraceMessage backtraceMessage){
+        BacktraceData backtraceData = backtraceMessage.getBacktraceData();
+
+        if (backtraceData == null) {
+            LOGGER.warn("BacktraceData in queue is null");
+            return;
+        }
+
+        this.database.saveReport(backtraceData);
+
+        BacktraceResult result = ApiSender.sendReport(config.getServerUrl(), backtraceData);
+
+        if (result.getStatus() == BacktraceResultStatus.Ok) {
+            backtraceData.getReport().markAsSent();
+            database.removeReport(backtraceData);
+        } else {
+            this.queue.add(backtraceMessage);
+        }
+
+        OnServerResponseEvent callback = backtraceMessage.getCallback();
+        if (callback != null) {
+            callback.onEvent(result);
+        }
+    }
+}
--- src/main/java/backtrace/io/BacktraceThread.java
+++ src/main/java/backtrace/io/BacktraceThread.java
@@ -1,7 +1,5 @@
 package backtrace.io;
 
-
-import backtrace.io.events.OnServerResponseEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,20 +8,15 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class BacktraceThread extends Thread {
     private static final transient Logger LOGGER = LoggerFactory.getLogger(BacktraceThread.class);
     private final static String THREAD_NAME = "backtrace-deamon";
-
-    private ConcurrentLinkedQueue<BacktraceMessage> queue;
-    private BacktraceDatabase database;
-    private final BacktraceConfig config;
-
+    private Backtrace backtrace;
 
     private BacktraceThread(BacktraceConfig config, ConcurrentLinkedQueue<BacktraceMessage> queue){
         super();
-        this.database = BacktraceDatabase.init(config, queue);
-        this.config = config;
-        this.queue = queue;
+        this.backtrace = new Backtrace(config, queue);
     }
 
     static void init(BacktraceConfig config, ConcurrentLinkedQueue<BacktraceMessage> queue){
+        LOGGER.info("Initialize BacktraceThread");
         BacktraceThread thread = new BacktraceThread(config, queue);
         thread.setDaemon(true);
         thread.setName(THREAD_NAME);
@@ -32,44 +25,6 @@ public class BacktraceThread extends Thread {
 
     @Override
     public void run(){
-        while(true){
-            BacktraceMessage message = queue.poll();
-
-            if (message == null) {
-                continue;
-            }
-
-            try {
-                pipeline(message);
-            }
-            catch (Exception e){
-                LOGGER.error("Exception during pipeline for message from queue..", e);
-            }
-        }
-    }
-
-    private void pipeline(BacktraceMessage backtraceMessage) {
-        BacktraceData backtraceData = backtraceMessage.getBacktraceData();
-
-        if (backtraceData == null) {
-            LOGGER.warn("BacktraceData in queue is null");
-            return;
-        }
-
-        database.saveReport(backtraceData);
-
-        BacktraceResult result = ApiSender.sendReport(config.getServerUrl(), backtraceData);
-
-        if (result.getStatus() == BacktraceResultStatus.Ok) {
-            backtraceData.getReport().markAsSent();
-            database.removeReport(backtraceData);
-        } else {
-            this.queue.add(backtraceMessage);
-        }
-
-        OnServerResponseEvent callback = backtraceMessage.getCallback();
-        if (callback != null) {
-            callback.onEvent(result);
-        }
+        backtrace.processBacktraceMessages();
     }
 }
