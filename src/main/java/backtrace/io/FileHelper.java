feb993ac023c4815b3589146337c9fb7035cac7c
Bartosz Litwiniuk <Bartosz.Litwiniuk@poczta.onet.pl>
2019-11-23 23:52:24 +0100

    Add database tests

--- src/main/java/backtrace/io/FileHelper.java
+++ src/main/java/backtrace/io/FileHelper.java
@@ -56,4 +56,29 @@ public class FileHelper {
         return new File(absoluteFilePath).exists();
     }
 
+
+    /**
+     *
+     * @param f
+     * @throws Exception
+     */
+    static void deleteRecursive(File f) throws Exception {
+        try {
+            if (!f.exists()){
+                return;
+            }
+            if (f.isDirectory()) {
+                for (File c : f.listFiles()) {
+                    deleteRecursive(c);
+                }
+            }
+            if (!f.delete()) {
+                throw new Exception("Delete command returned false for file: " + f);
+            }
+        }
+        catch (Exception e) {
+            throw new Exception("Failed to delete the folder: " + f, e);
+        }
+    }
+
 }
--- src/test/java/backtrace/io/BacktraceDatabaseTest.java
+++ src/test/java/backtrace/io/BacktraceDatabaseTest.java
@@ -2,6 +2,7 @@ package backtrace.io;
 
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
@@ -9,7 +10,31 @@ import java.util.LinkedList;
 
 public class BacktraceDatabaseTest {
 
-    private final String databasePath = "./backtrace_test/";
+    private final String databasePath = "backtrace_test/";
+    private final String message = "message";
+
+    private BacktraceConfig backtraceConfig;
+    private LinkedList<BacktraceMessage> queue;
+    private BacktraceData backtraceData;
+
+    @Before
+    public void init() throws Exception{
+        this.cleanDatabaseDir();
+        // GIVEN
+        this.backtraceConfig = new BacktraceConfig("", "");
+        this.backtraceConfig.setDatabasePath(databasePath);
+        this.queue = new LinkedList<>();
+        this.backtraceData = new BacktraceData(new BacktraceReport(this.message));
+    }
+
+    /**
+     * Remove database dir after each test
+     */
+    @After
+    public void cleanDatabaseDir() throws Exception {
+        File file = new File(databasePath);
+        FileHelper.deleteRecursive(file);
+    }
 
     @Test(expected = NullPointerException.class)
     public void invalidNullBacktraceConfig() {
@@ -32,24 +57,55 @@ public class BacktraceDatabaseTest {
         // GIVEN
         BacktraceConfig config = new BacktraceConfig("", "");
         config.setDatabasePath(databasePath);
-        LinkedList<BacktraceMessage> queue = new LinkedList<>();
 
         // WHEN
-        BacktraceDatabase database = BacktraceDatabase.init(config, queue);
+        BacktraceDatabase database = BacktraceDatabase.init(config, this.queue);
 
         // THEN
         Assert.assertEquals(0, database.size());
         Assert.assertEquals(0, queue.size());
     }
 
-    /**
-     * Remove database dir after each test
-     */
-    @After
-    public void cleanDatabaseDir(){
-        File file = new File(databasePath);
-        if(file.exists()){
-            file.delete();
-        }
+
+
+
+    @Test
+    public void saveReportToDatabase() {
+        // GIVEN
+        BacktraceDatabase database = BacktraceDatabase.init(backtraceConfig, queue);
+
+        // WHEN
+        database.saveReport(backtraceData);
+        // THEN
+        Assert.assertEquals(1, database.size());
+        Assert.assertEquals(0, queue.size());
+    }
+
+    @Test
+    public void saveAndLoadReport() {
+        // GIVEN
+        BacktraceDatabase database = BacktraceDatabase.init(this.backtraceConfig, this.queue);
+
+        // WHEN
+        database.saveReport(this.backtraceData);
+        database = BacktraceDatabase.init(this.backtraceConfig, this.queue);
+
+        // THEN
+        Assert.assertEquals(1, database.size());
+        Assert.assertEquals(1, this.queue.size());
+        Assert.assertEquals(this.message, this.queue.getFirst().getBacktraceData().getReport().getMessage());
+    }
+
+    @Test
+    public void saveAndDelete() {
+        // GIVEN
+        BacktraceDatabase database = BacktraceDatabase.init(this.backtraceConfig, this.queue);
+
+        // WHEN
+        database.saveReport(this.backtraceData);
+        database.removeReport(this.backtraceData);
+
+        // THEN
+        Assert.assertEquals(0, database.size());
     }
 }
