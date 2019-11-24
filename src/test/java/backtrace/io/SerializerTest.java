c634c616f2013a6a0c7df898a4d9864aec06c139
Bartosz Litwiniuk <Bartosz.Litwiniuk@poczta.onet.pl>
2019-11-24 14:27:57 +0100

    Add tests for serializer

--- src/test/java/backtrace/io/SerializerTest.java
+++ src/test/java/backtrace/io/SerializerTest.java
@@ -1,7 +1,47 @@
 package backtrace.io;
 
-/**
- * Created by Stacja on 2019-11-24.
- */
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class SerializerTest {
+    private final String message = "test-message";
+    private String expectedJson;
+
+    class MockTestObject {
+        private String message;
+
+        MockTestObject(String message){
+            this.message = message;
+        }
+
+        public String getMessage(){
+            return this.message;
+        }
+    }
+
+    @Before
+    public void init(){
+        this.expectedJson = String.format("{\"message\":\"%s\"}", this.message);
+    }
+
+    @Test
+    public void objectSerialization() {
+        // GIVEN
+        MockTestObject mto = new MockTestObject(this.message);
+        // WHEN
+        String json = BacktraceSerializeHelper.toJson(mto);
+        // THEN
+        Assert.assertEquals(expectedJson, json);
+    }
+
+    @Test
+    public void objectDeserialization() {
+        // WHEN
+        MockTestObject mto = BacktraceSerializeHelper.fromJson(expectedJson, MockTestObject.class);
+
+        // THEN
+        Assert.assertNotNull(mto);
+        Assert.assertEquals(this.message, mto.getMessage());
+    }
 }
