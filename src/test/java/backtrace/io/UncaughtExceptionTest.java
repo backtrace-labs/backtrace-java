e62883559e0fceb140b81944e2585170fd2c8fbe
Bartosz Litwiniuk <bartosz.litwiniuk@poczta.onet.pl>
2020-02-03 18:34:02 +0100

    Reduction of waiting time in the test

--- src/test/java/backtrace/io/UncaughtExceptionTest.java
+++ src/test/java/backtrace/io/UncaughtExceptionTest.java
@@ -141,7 +141,7 @@ public class UncaughtExceptionTest {
         // THEN
         try {
             testThread.join();
-            waiter.await(3000, TimeUnit.SECONDS);
+            waiter.await(5, TimeUnit.SECONDS);
         } catch (InterruptedException | TimeoutException exception) {
             Assert.fail(exception.getMessage());
         }
