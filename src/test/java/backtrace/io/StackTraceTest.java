8230319b1659eece024834c043b0c9cc8ac51f86
Bartosz Litwiniuk <bartosz.litwiniuk@poczta.onet.pl>
2020-02-13 20:03:54 +0100

    Add test for filtering out backtrace stack frames

--- src/test/java/backtrace/io/StackTraceTest.java
+++ src/test/java/backtrace/io/StackTraceTest.java
@@ -1,4 +1,28 @@
 package backtrace.io;
 
+import backtrace.io.data.report.BacktraceStackFrame;
+import backtrace.io.data.report.BacktraceStackTrace;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class StackTraceTest {
+
+    @Test
+    public void removeIncorrectPathsFromNull() {
+        // GIVEN
+        Exception testException = new Exception();
+        StackTraceElement[] stackTraceElements = new StackTraceElement[2];
+        stackTraceElements[0] = new StackTraceElement("BacktraceTest", "test", "test", 1);
+        stackTraceElements[1] = new StackTraceElement("Demo", "test", "test", 1);
+        testException.setStackTrace(stackTraceElements);
+
+        // WHEN
+        BacktraceStackTrace backtraceStackTrace = new BacktraceStackTrace(testException);
+
+        // THEN
+        Assert.assertEquals(backtraceStackTrace.getStackFrames().size(), 1);
+    }
 }
