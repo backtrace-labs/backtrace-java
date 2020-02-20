e14fbb23d8fcf8950ad719b35976afbb5098cf05
Bartosz Litwiniuk <bartosz.litwiniuk@poczta.onet.pl>
2020-02-20 18:00:54 +0100

    Update Demo app

--- src/demo/src/app/Demo.java
+++ src/demo/src/app/Demo.java
@@ -5,15 +5,10 @@ import backtrace.io.BacktraceConfig;
 import backtrace.io.data.BacktraceReport;
 
 public class Demo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         BacktraceReport report = new BacktraceReport("test message");
         BacktraceClient client = new BacktraceClient(new BacktraceConfig("<endpoint url>", "<submission token>"));
         client.send(report);
-        try {
-            report.await(); // wait for sending report
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
+        client.await();
     }
 }
